a280e6026738e9c6a076afa2c4b08c2e2e09c4d2
dylangomez <dylangomezvazquez@hotmail.com>
2017-12-13 14:08:39 +0100

    Test 3 to 5 implemnted, started with test 5

--- Rover.py
+++ Rover.py
@@ -1,3 +1,4 @@
+from random import randint
 """
     Test 1: Rover moves forward 1 step to show it will move
 
@@ -10,6 +11,7 @@ yAxis = 0
 facingDirection = "North"
 
 command = input("What does the Rover need to do?: ")
+
 #
 # for letter in command:
 #     if(letter == "F"):
@@ -31,20 +33,211 @@ command = input("What does the Rover need to do?: ")
     
 """
 # Is getting a bit crazy with every elif, need to find better fix next test/ iteration
+# Test does succeed, so yay for now.
+
+# for letter in command:
+#     if(letter == "F" and facingDirection == "North"):
+#         xAxis += 1
+#     elif(letter == "R" and facingDirection == "North"):
+#         facingDirection = "East"
+#     elif(letter == "F" and facingDirection == "East"):
+#         yAxis += 1
+#     elif (letter == "F" and facingDirection == "South"):
+#         xAxis -= 1
+#     elif (letter == "F" and facingDirection == "East"):
+#         yAxis -= 1
+#
+#
+# print("Rover is at position:", xAxis, ",", yAxis ,"facing", facingDirection )
+#
+
+"""
+    Test 4: check where the borders are in the grid and check if code continues correctly
+    
+    Input: BRFFR
+    
+    Output: "Oops, you can't go here, you are set back to your previous location"
+    Output: Rover is at position 0 , 2 facing East
+    
+"""
+
+# for letter in command:
+#
+#     if(letter == "B" and xAxis == 0 or letter == "F" and xAxis == 100):
+#         print("Oops, you can't go here, you are set back to your previous location")
+#
+#     else:
+#         if(letter == "F" and facingDirection == "North"):
+#             xAxis += 1
+#
+#         elif(letter == "R" and facingDirection == "North"):
+#             facingDirection = "East"
+#
+#         elif(letter == "F" and facingDirection == "East"):
+#             yAxis += 1
+#
+#         elif (letter == "F" and facingDirection == "South"):
+#             xAxis -= 1
+#
+#         elif (letter == "F" and facingDirection == "East"):
+#             yAxis -= 1
+#
+# print("Rover is at position:", xAxis, ",", yAxis ,"facing", facingDirection)
+
+
+"""
+    Test 5: Add command "B" to movement of rover
+
+    Input: FFFBB
+
+    Output: Rover is at position 0 , 1 facing North
+
+"""
 for letter in command:
-    if(letter == "F" and facingDirection == "North"):
-        xAxis += 1
-    elif(letter == "R" and facingDirection == "North"):
-        facingDirection = "East"
-    elif(letter == "F" and facingDirection == "East"):
-        yAxis += 1
-    elif (letter == "F" and facingDirection == "South"):
-        xAxis -= 1
-    elif (letter == "F" and facingDirection == "East"):
-        yAxis -= 1
 
 
-print("Rover is at position:", xAxis, ",", yAxis ,"facing", facingDirection )
+    if (letter == "B" and xAxis == 0 or letter == "F" and xAxis == 100):
+        print("Oops, you can't go here, you are set back to your previous location")
+    elif():
+        print("")
+    else:
+        # Movement with command F
+        if (letter == "F" and facingDirection == "North"):
+            xAxis += 1
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis += 1
+
+        elif (letter == "F" and facingDirection == "South"):
+            xAxis -= 1
+
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis -= 1
+
+        # Movement with command B
+        elif (letter == "B" and facingDirection == "North"):
+            xAxis -= 1
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis -= 1
+
+        elif (letter == "F" and facingDirection == "South"):
+            xAxis += 1
+
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis += 1
+
+        # Changing directions with command: R
+        elif (letter == "R" and facingDirection == "North"):
+            facingDirection = "East"
+        elif (letter == "R" and facingDirection == "East"):
+            facingDirection = "South"
+        elif (letter == "R" and facingDirection == "South"):
+            facingDirection = "West"
+        elif (letter == "R" and facingDirection == "West"):
+            facingDirection = "North"
+
+        # Changing directions with command: L
+        elif (letter == "L" and facingDirection == "North"):
+            facingDirection = "West"
+        elif (letter == "R" and facingDirection == "East"):
+            facingDirection = "North"
+        elif (letter == "R" and facingDirection == "South"):
+            facingDirection = "East"
+        elif (letter == "R" and facingDirection == "West"):
+            facingDirection = "South"
+
+
+
+print("Rover is at position:", xAxis, ",", yAxis, "facing", facingDirection)
+
+
+
+
+
+""" 
+    Test 6: Add obstacles and detect them
+    
+    Input: FFFFRFFF
+    
+    Output: "Obstacle detected moving back to previous position"
+
+"""
+
+#Making obstacles
+obstacleXAxis = []
+obstacleYAxis = []
+
+# Max amount of obstacles
+amountObstacles = randint(0, 10)
+
+for i in range (0, amountObstacles):
+    # Location of the obstacles
+    # Check if I is uneven or even to spread the obstacles around the grid
+    # In future iteration this can get changed.
+
+    if(i % 2 == 0):
+        obstacleXAxis.append(randint(0,10))
+    else:
+        obstacleYAxis.append(randint(0,10))
+
+#Check every command that is given
+
+for letter in command:
+
+
+    if (letter == "B" and xAxis == 0 or letter == "F" and xAxis == 100):
+        print("Oops, you can't go here, you are set back to your previous location")
+    elif():
+        print("")
+    else:
+        # Movement with command F
+        if (letter == "F" and facingDirection == "North"):
+            xAxis += 1
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis += 1
+
+        elif (letter == "F" and facingDirection == "South"):
+            xAxis -= 1
+
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis -= 1
+
+        # Movement with command B
+        elif (letter == "B" and facingDirection == "North"):
+            xAxis -= 1
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis -= 1
+
+        elif (letter == "F" and facingDirection == "South"):
+            xAxis += 1
+
+        elif (letter == "F" and facingDirection == "East"):
+            yAxis += 1
+
+        # Changing directions with command: R
+        elif (letter == "R" and facingDirection == "North"):
+            facingDirection = "East"
+        elif (letter == "R" and facingDirection == "East"):
+            facingDirection = "South"
+        elif (letter == "R" and facingDirection == "South"):
+            facingDirection = "West"
+        elif (letter == "R" and facingDirection == "West"):
+            facingDirection = "North"
+
+        # Changing directions with command: L
+        elif (letter == "L" and facingDirection == "North"):
+            facingDirection = "West"
+        elif (letter == "R" and facingDirection == "East"):
+            facingDirection = "North"
+        elif (letter == "R" and facingDirection == "South"):
+            facingDirection = "East"
+        elif (letter == "R" and facingDirection == "West"):
+            facingDirection = "South"
+
+
+
+print("Rover is at position:", xAxis, ",", yAxis, "facing", facingDirection)
+
+
 
 
 
